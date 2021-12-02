e29ef2910859358c2304cc2f3463fc60c7a7f316
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-12-02 14:40:52 -0300

    <feat> implementando metodo de ataque e ataque especial

--- app.js
+++ app.js
@@ -16,9 +16,24 @@ new Vue({
          this.running = true
          this.playerLife = 100
          this.monsterLife = 100
+      },
+      attack(especial) {
+         this.hurt('monsterLife', 5, 10, especial)
+         this.hurt('playerLife', 7, 12, false)
+      },
+      getRandom(min, max) {
+         const value = Math.random() * (max - min) + min
+         return Math.round(value)
+      },
+      hurt(atr, min, max, especial) {
+         const plus = especial ? 5 : 0
+         const hurt = this.getRandom(min + plus, max + plus)
+         this[atr] = Math.max(this[atr] - hurt, 0)
       }
    },
    watch: {
-
+      hasResult(value) {
+         if(value) this.running = false
+      }
    }
 })
