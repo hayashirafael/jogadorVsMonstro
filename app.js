ae58c4f3af371309d3b5669f4b5d0d2697dac42c
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-12-02 15:47:48 -0300

    <feat> implementando funcionalidade de aparecer log

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
       running: false,
       playerLife: 100,
       monsterLife: 100,
+      logs: []
 
    },
    computed: {
@@ -18,17 +19,20 @@ new Vue({
          this.monsterLife = 100
       },
       attack(especial) {
-         this.hurt('monsterLife', 5, 10, especial)
-         this.hurt('playerLife', 7, 12, false)
+         this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+         if(this.monsterLife > 0) {
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+         }
       },
       getRandom(min, max) {
          const value = Math.random() * (max - min) + min
          return Math.round(value)
       },
-      hurt(atr, min, max, especial) {
+      hurt(atr, min, max, especial, source, target, cls) {
          const plus = especial ? 5 : 0
          const hurt = this.getRandom(min + plus, max + plus)
          this[atr] = Math.max(this[atr] - hurt, 0)
+         this.registerLog(`${source} atingiu ${target} com ${hurt}`, cls)
       },
       healAndHurt() {
          this.heal(10, 15)
@@ -37,6 +41,9 @@ new Vue({
       heal(min, max) {
          const heal = this.getRandom(min, max)
          this.playerLife = Math.min(this.playerLife + heal, 100)
+      },
+      registerLog(text, cls) {
+         this.logs.unshift({text, cls})
       }
    },
    watch: {
