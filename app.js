27e5471752225d60a115fdd6da56fda9f2a7a15a
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-12-02 11:31:53 -0300

    mostrando e estilizando o resultado

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 new Vue({
    el: '#app',
    data: {
-      playerLife: 15,
-      monsterLife: 70,
+      playerLife: 100,
+      monsterLife: 0,
 
    },
    computed: {
-
+      hasResult() {
+         return this.playerLife === 0 || this.monsterLife === 0
+      }
    },
    methods: {
       
