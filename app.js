73e4edc17a3bc0b061bc8a9a7e990d4dc0c1b771
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-12-02 16:59:02 -0300

    <style> estilizando log

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ new Vue({
          this.running = true
          this.playerLife = 100
          this.monsterLife = 100
+         this.logs = []
       },
       attack(especial) {
          this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
@@ -32,15 +33,16 @@ new Vue({
          const plus = especial ? 5 : 0
          const hurt = this.getRandom(min + plus, max + plus)
          this[atr] = Math.max(this[atr] - hurt, 0)
-         this.registerLog(`${source} atingiu ${target} com ${hurt}`, cls)
+         this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
       },
       healAndHurt() {
          this.heal(10, 15)
-         this.hurt('playerLife', 7, 12, false)
+         this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
       },
       heal(min, max) {
          const heal = this.getRandom(min, max)
          this.playerLife = Math.min(this.playerLife + heal, 100)
+         this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
       },
       registerLog(text, cls) {
          this.logs.unshift({text, cls})
